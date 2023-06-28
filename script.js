aaa5a599427806c59e781c1682a978aedb574343
EHXSmallStone <127904795+EHXSmallStone@users.noreply.github.com>
2023-06-27 22:21:30 -0300

    Add a hover effect to the grid

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const gridContainer = document.querySelector('.gridContainer');
 
 for(i = 1; i <= 256; i++) {
   let square = document.createElement('div');
-  square.classList.add("square");
+  square.classList.add('square');
   gridContainer.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+for (let square of squares) {
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'black';
+  })
+};
