384ae2810b9ed010a942ff8cc27b63a3f83f8b77
EHXSmallStone <127904795+EHXSmallStone@users.noreply.github.com>
2023-06-28 01:05:18 -0300

    Add button to change the grid

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const gridContainer = document.querySelector('.gridContainer');
+const gridContainer = document.querySelector('#gridContainer');
 
 for(i = 1; i <= 256; i++) {
   let square = document.createElement('div');
+  square.style.width = "6.25%"
   square.classList.add('square');
   gridContainer.appendChild(square);
 }
 
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
 for (let square of squares) {
   square.addEventListener('mouseover', () => {
     square.style.backgroundColor = 'black';
   })
 };
+
+const btnChangeGrid = document.querySelector('#changeGrid');
+btnChangeGrid.addEventListener('click', () => {
+  let answer = +prompt('Enter the number of squares per side for the grid', '16');
+  if (answer > 100) {
+    alert('It\'s too much! The maximum value is 100')
+    return;
+  }
+
+  for(let square of squares) {
+    gridContainer.removeChild(square);
+  }
+
+  for(i = 1; i <= answer * answer; i++) {
+    let square = document.createElement('div');
+    square.style.width = `${100 / answer}%`;
+    square.classList.add('square');
+    gridContainer.appendChild(square);
+  }
+
+  squares = document.querySelectorAll('.square');
+  for (let square of squares) {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    })
+  };
+});
